127d602abf97cece4b59edfdf851711aee8d30d6
Dianwen Li <dianwen@gmail.com>
2015-05-07 19:54:57 -0400

    Added evaluation for HLT, PSH, POP, and ADD instructions

--- src/main.c
+++ src/main.c
@@ -2,14 +2,45 @@
 #include "instructions.h"
 #include "program.h"
 
+static const int stackSize = 256;
 
+bool running = 1;
 int pc = 0;
+int sp = -1;
+int stack[stackSize];
 
 int fetch() {
   return program[pc];
 }
 
+void eval(int instruction) {
+  switch (instruction) {
+    case HLT: {
+      running = false;
+      break;
+    }
+    case PSH:  {
+      stack[++sp] = program[++ip];
+      break;
+    }
+    case POP: {
+      int value = stack[sp--];
+      printf("Popped %d\n", value);
+    }
+    case ADD: {
+      int value1 = stack[sp--];
+      int value2 = stack[sp--];
+      int sum = value1 + value2;
+      stack[++sp] = sum;
+      break;
+    }
+  }
+}
+
 int main() {
-  printf("%d", program[0]);
+  while(running) {
+    eval(fetch());
+    ip++;
+  }
   return 0;
 }
