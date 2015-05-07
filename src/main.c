1495c4e049d2baf7bfa725cbaec70f4dfe8f3881
Dianwen Li <dianwen@gmail.com>
2015-05-07 19:25:04 -0400

    Added sample program and separated code into header files

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include "instructions.h"
+#include "program.h"
+
+
+int pc = 0;
+
+int fetch() {
+  return program[pc];
+}
+
 int main() {
+  printf("%d", program[0]);
   return 0;
 }
